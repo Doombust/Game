c682e0968ad6772d10409fefb80b25850e8c6d27
Patrick King <pat.k195@gmail.com>
2024-01-26 10:18:33 -0500

    prints console log with test_Import

---
+++ Content/Python/import_unreal.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1311a4ce1d9b6e91c13b572da8ae3affa67a867b29ffa1aa0371b5fad8f2e75
+size 42
---
+++ Content/Python/test_Import.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9afa37fd4acba708face3fd00533eea2930db0dcdb50e414ab0d0f5a9d210464
+size 36
